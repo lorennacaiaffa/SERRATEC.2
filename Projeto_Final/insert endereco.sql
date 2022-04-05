03f3b5b7a25396e2660091237d13fce5ecccb593
Luis Filipe <lfilipewborges@gmail.com>
2022-04-05 12:33:21 -0300

    insert clinete, edereco

---
+++ Projeto_Final/insert endereco.sql
@@ -0,0 +1,35 @@
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('28630487', 'Carlos Condack', 'São Geraldo', 'Nova Friburgo', 'RJ')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('28614090', 'RauSertan', 'Parque Santa Eliza', 'Nova Friburgo', 'RJ')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('22159385', 'São João', 'Industrial', 'Palmas', 'TO')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('49519352', 'Sete de Setembro', 'Centro', 'Manaus', 'AM')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('68176671', 'São Pedro', 'São José', 'São Paulo', 'SP')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('52275644', 'Vinte e três', 'Planalto', 'Fortaleza', 'CE')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('59665324', 'Dois', 'São Francisco', 'Patos', 'PB')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('69270239', 'Treze de Maio', 'São Francisco', 'Teresina', 'PI')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('60077520', 'Rui Barbosa', 'Aeroporto 10', 'São Francisco', 'RS')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('29145672', 'Alagoas', 'São José', 'São Gonçalo', 'RN')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('2800000', 'antonio thuler','olaria', 'NovaFriburgo','RJ')
+
+insert into endereco (cep, rua, bairro, cidade, estado)
+values ('25957005', 'acre','araras', 'Teresópolis','RJ');
